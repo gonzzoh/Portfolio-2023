005321d0616e0406cceac64c5009215f1b069606
gonzzoh <gmeza0118@gmail.com>
2023-11-14 18:27:49 -0500

    CSS and Images Integrated

--- src/components/aboutme.jsx
+++ src/components/aboutme.jsx
@@ -1,13 +1,21 @@
 export default function AboutMe() {
     return (
-        <section id="aboutMe">
-            <div id="aboutMeContainer">
-                <span class="textWrapper">
-                    <h1>Hi, I'm Gonzalo</h1>
-                    <h2>Here's what I bring to the table.</h2>
-                    <h3>Based in Roselle Park, New Jersey lol</h3>
-                </span>
+        <div id="aboutMeContainer">
+            <h1 id="aboutMeTitle">Know Me More!</h1>
+            <div id="personalInfoContainer">
+                <div id="bio">
+                    <h2>I'm Gonzalo Meza-Cabrera, a full-stack software engineer</h2>
+                    <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Illum enim aspernatur placeat delectus adipisci est omnis dicta iure rerum architecto dolorum eum consequuntur beatae, quibusdam eveniet possimus a sunt. Quisquam! Lorem ipsum, dolor sit amet consectetur adipisicing elit. Non veniam alias fugit esse quia totam consequatur inventore ipsam quisquam, nulla reiciendis, excepturi eaque cum optio, distinctio in incidunt facere porro! Lorem ipsum dolor sit amet consectetur adipisicing elit. Iusto id esse quia quas voluptas voluptatem modi, corrupti, molestiae ipsum quod blanditiis voluptatum aperiam quam alias sint consequuntur eum iste! Voluptatum.
+                    </p>
+                </div>
+
+                <ul id="personalInfo">
+                    <li>Name: Gonzalo Meza-Cabrera</li>
+                    <li>Email: gonzalofmezac@gmail.com</li>
+                    <li>Age: 20</li>
+                </ul>
+                <a href="N/A"></a>
             </div>
-        </section>
+        </div>
     );
 }
--- src/components/sidebar.jsx
+++ src/components/sidebar.jsx
@@ -1,9 +1,11 @@
+import chad from "../images/chad1-chad.gif"
+
 export default function SideBar() {
   return (
     <section id="sideBar">
       <div class="nameTitleContainer">
         <span class="imageWrapper">
-          <img src="N/A" placeholder="hihihi" />
+          <img src={chad} placeholder="hihihi" class="profileImg" />
         </span>
         <h2>Gonzalo Meza-Cabrera</h2>
       </div>
