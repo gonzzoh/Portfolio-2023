70d8293fbec6fd44b5634cb3e279a25f7da42d40
gonzzoh <gmeza0118@gmail.com>
2023-11-28 16:05:52 -0500

    css tweaks

--- src/components/landing.jsx
+++ src/components/landing.jsx
@@ -2,15 +2,13 @@
 
 export default function Landing() {
     return (
-        <section id="landing">
             <div id="landingContainer">
-                <span class="textWrapper">
+                <span class="landingTextWrapper">
                     <h1>Hi, I'm Gonzalo Meza-Cabrera, a full-stack software engineer</h1>
-                    <h2>Here's what I bring to the table.</h2>
-                    <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Illum enim aspernatur placeat delectus adipisci est omnis dicta iure rerum architecto dolorum eum consequuntur beatae, quibusdam eveniet possimus a sunt. Quisquam! Lorem ipsum, dolor sit amet consectetur adipisicing elit. Non veniam alias fugit esse quia totam consequatur inventore ipsam quisquam, nulla reiciendis, excepturi eaque cum optio, distinctio in incidunt facere porro! Lorem ipsum dolor sit amet consectetur adipisicing elit. Iusto id esse quia quas voluptas voluptatem modi, corrupti, molestiae ipsum quod blanditiis voluptatum aperiam quam alias sint consequuntur eum iste! Voluptatum.
+                    {/* <h2>Here's what I bring to the table.</h2> */}
+                    <p>I am a Full-Stack Software Engineer who specializes in back-end development and structuring data, while still maintaining a deep passion for intuitive and appealing UI design. I am enthusiastic to work as a part of a team and tackle big problems in organized and creative ways. I want to take full advantage of my early introduction into the professional world and work on as many projects as possible in order to expand my software engineering skillset and create innovative code. 
                     </p>
                 </span>
             </div>
-        </section>
     );
 }
--- src/components/sidebar.jsx
+++ src/components/sidebar.jsx
@@ -3,16 +3,16 @@ import chad from "../images/chad1-chad.gif"
 export default function SideBar() {
   return (
     <section id="sideBar">
-      <div class="nameTitleContainer">
+      <div class="sideBarContent">
         <span class="imageWrapper">
           <img src={chad} placeholder="hihihi" class="profileImg" />
         </span>
-        <h2>Gonzalo Meza-Cabrera</h2>
+        {/* <h2 class="nameTitle">Gonzalo Meza-Cabrera</h2> */}
         <section id="scrollOptions">
-          <a href="#landingContainer">Home</a>
-          <a href="#skillsContainer">Skills</a>
-          <a href="#portfolio">Portfolio</a>
-          <a href="#resume">Resume</a>
+          <a href="#landingContainer" className="sidebarLink">Home</a>
+          <a href="#skillsContainer" className="sidebarLink">Skills</a>
+          <a href="#portfolio" className="sidebarLink">Portfolio</a>
+          <a href="#resume" className="sidebarLink">Resume</a>
         </section>
       </div>
     </section>
